3a59193284687b2ba8318be0cbcb3ab5d923585d
Xavier Poinas <xavier.poinas@gmail.com>
2016-02-19 17:21:55 +0700

    Fixed parsing router MAC address on Windows (again!)

--- wifi-control.js
+++ wifi-control.js
@@ -663,7 +663,7 @@
                     break;
                   }
               }
-              if (KEY === "SSID") {
+              if (KEY === "BSSID") {
                 break;
               }
             }
@@ -691,7 +691,7 @@
                 case "SSID":
                   interfaceState.ssid = VALUE;
               }
-              if (KEY === "BSSID") {
+              if (KEY === "SSID") {
                 break;
               }
             }
